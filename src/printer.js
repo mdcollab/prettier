ef08f51fd3ade9b2ce2f89f2c56dd403b39f117a
Tom Berry <tberry860@gmail.com>
2017-03-26 10:30:13 -0700

    sort 'store' to the end

--- src/printer.js
+++ src/printer.js
@@ -57,6 +57,7 @@ const getPropSortNumber = (name, i) => {
   if (index !== -1) return index;
   if (name.search("^on[A-Z]") !== -1) return i + 200;
   if (name.search("^all[A-Z]") !== -1) return i + 300;
+  if (name === "store") return i + 400;
   return i + 100;
 };
 
