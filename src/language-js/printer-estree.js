57830a13d2ccbcbd2c336ba38556b86250cd22b8
Tom Berry <tberry860@gmail.com>
2019-06-12 17:18:13 -0400

    don't break type parameter lists if there's only one parameter

--- src/language-js/printer-estree.js
+++ src/language-js/printer-estree.js
@@ -4643,13 +4643,7 @@ function printTypeParameters(path, options, print, paramsKey) {
   const shouldInline =
     isParameterInTestCall ||
     n[paramsKey].length === 0 ||
-    (n[paramsKey].length === 1 &&
-      (shouldHugType(n[paramsKey][0]) ||
-        (n[paramsKey][0].type === "GenericTypeAnnotation" &&
-          shouldHugType(n[paramsKey][0].id)) ||
-        (n[paramsKey][0].type === "TSTypeReference" &&
-          shouldHugType(n[paramsKey][0].typeName)) ||
-        n[paramsKey][0].type === "NullableTypeAnnotation"));
+    n[paramsKey].length === 1;
 
   if (shouldInline) {
     return concat(["<", join(", ", path.map(print, paramsKey)), ">"]);
